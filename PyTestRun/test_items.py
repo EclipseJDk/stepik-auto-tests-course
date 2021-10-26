ba7d76676fdc9f902d3c92ea9e7603ff3c1f403e
eclipse <eclipse0044@gmail.com>
2021-10-26 17:03:04 +0300

    update

--- PyTestRun/test_items.py
+++ PyTestRun/test_items.py
@@ -1,9 +1,11 @@
 import time
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 
 def test_find_add_to_card_btn(browser):
-    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    add_to_basket = browser.find_element(By.ID, "add_to_basket_form")
+    browser.get(link)
+    add_to_basket = browser.find_elements(By.ID, "add_to_basket_form")
     time.sleep(4)
-    assert add_to_basket.is_displayed(), "There is no button for adding an item to the cart "
+    assert add_to_basket, "There is no button for adding an item to the cart "
